670f0e7ff8bce44377dd223892f1aaa8c82844ec
Varsha  U K <varshauk29@gmail.com>
2025-05-20 16:00:03 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  //  THIS tells Angular to use <app-root>
+  imports: [HeaderComponent,UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'first-angular-app';
 }
+// So when Angular sees <app-root></app-root> in index.html, it:
+// Finds the AppComponent class.
+// Loads its HTML (from app.component.html).
+// Replaces <app-root> with that HTML.
+
--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,13 @@ import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+
+// This function:
+
+// Starts the Angular app using AppComponent as the root.
+
+// Uses appConfig for setting up services like routing or HTTP.
+
+// If there's an error during the bootstrap process, it will catch it and log it using console.error().
+
